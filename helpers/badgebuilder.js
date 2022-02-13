035913f1bb52c30de34b36c58e0fac6394573fdb
Arne Wendt <arne.wendt@tuhh.de>
2022-02-13 21:40:43 +0100

    build badges from matrix outputs; store svg in gist

---
+++ helpers/badgebuilder.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const { badgen } = require('badgen');
+
+const { createSVGWindow } = require('svgdom');
+const window = createSVGWindow();
+const document = window.document;
+const { SVG, registerWindow } = require('@svgdotjs/svg.js');
+registerWindow(window, document);
+
+const offset = 5;
+
+// read [ { job: <name>, success: <truthy> }] from stdin
+const matrix = JSON.parse(fs.readFileSync(process.stdin.fd, 'utf-8'));
+
+// build badges
+const badges = matrix
+    .map((job) => {
+        return badgen({
+            label: job.job,
+            status: job.success ? 'passing' : 'failing',
+            color: job.success ? 'green' : 'red',
+            style: 'flat'
+        });
+    })
+    .reduce((doc, badge, idx) => {
+        const badge_svg = SVG(badge);
+        doc.add(badge_svg.move((doc.bbox().width - doc.bbox().x) + (idx ? 1 : 0)*offset, 0));
+
+        return doc;
+    }, SVG());
+
+// print horizontal badge row svg to stdout
+console.log(badges.svg());
